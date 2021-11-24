533704ee30feff7cba096306b361eea404c64123
NikitaKharashun <kharashun.nikita73@gmail.com>
2021-11-24 21:06:30 +0300

    Добавьте файлы проекта.

---
+++ Nikita_SpyProgram/Elements/SpyFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikita_SpyProgram.Elements
+{
+    class SpyFile
+    {
+        public event Action<DateTime, ConsoleKeyInfo> KeyLogger;
+
+        public void Start()
+        {
+            bool isEnter = false;
+            ConsoleKeyInfo key;
+
+            Console.WriteLine("Включено логирование клавиш");
+
+            while (!isEnter)
+            {
+                key = Console.ReadKey();
+
+                isEnter = key.Key.Equals(ConsoleKey.Enter);
+
+                KeyLogger(DateTime.Now, key);
+            }
+        }
+    }
+}
---
+++ Nikita_SpyProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nikita_SpyProgram.Elements;
+
+namespace Nikita_SpyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spyFile = new SpyFile();
+
+            Dictionary<DateTime, ConsoleKeyInfo> keys = new Dictionary<DateTime, ConsoleKeyInfo>();
+
+            spyFile.KeyLogger += (date, info) => keys[date] = info;
+
+            spyFile.Start();
+
+            foreach (KeyValuePair<DateTime, ConsoleKeyInfo> item in keys)
+                Console.WriteLine($" Время: {item.Key} | Клавиша : {item.Value.KeyChar}");
+
+            Console.ReadLine();
+        }
+    }
+}
